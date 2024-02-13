d79d6293b551dacab31109b7bb0cb65711314834
Helene <helene.quintard@lapiscine.pro>
2024-02-13 14:03:28 +0100

    Add Header + CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Header from './components/Header';
 
 function App() {
 
   return (
-<div>Hello</div>
+    <Header />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+const Header = () => {
+    return (
+        <header>
+        <nav>
+          <ul>
+            <li> 
+              <a  href="#home">
+                  Home
+              </a>
+            </li>
+            <li> 
+              <a  href="#about">
+                  About
+              </a>
+            </li>
+            <li> 
+              <a  href="#project">
+                  Project
+              </a>
+            </li>
+            <li> 
+              <a  href="#contact">
+                  Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+
+export default Header;
+
+// freelogodesign
